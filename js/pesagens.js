1b983eb74505a92d277e3940880bf46a161dabf9
Alexrocha666 <alexpinposo@gmail.com>
2026-02-21 00:01:19 -0300

    Add methods to manage pesagens for tanks

--- js/pesagens.js
+++ js/pesagens.js
@@ -1,1 +1,23 @@
+const Pesagens = {
 
+  async adicionar(tanqueId) {
+    const peso = parseFloat(prompt("Digite o peso médio atual (g):"));
+    if (!peso) return;
+
+    await db.pesagens.add({
+      tanqueId,
+      data: new Date(),
+      pesoMedio: peso
+    });
+
+    Tanques.render();
+  },
+
+  async listar(tanqueId) {
+    return await db.pesagens
+      .where("tanqueId")
+      .equals(tanqueId)
+      .toArray();
+  }
+
+};
