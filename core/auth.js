7ba349df523f61209fe5192a53c403cb0e2e25f8
Alexrocha666 <alexpinposo@gmail.com>
2026-02-23 22:37:30 -0300

    Create auth.js

---
+++ core/auth.js
@@ -0,0 +1,70 @@
+let usuarioAtual = null;
+
+function telaLogin() {
+  document.getElementById("app").innerHTML = `
+    <h2>Login</h2>
+    <input id="email" placeholder="Email"><br><br>
+    <input id="senha" type="password" placeholder="Senha"><br><br>
+    <button onclick="login()">Entrar</button>
+    <button onclick="telaCadastro()">Cadastrar</button>
+  `;
+}
+
+function telaCadastro() {
+  document.getElementById("app").innerHTML = `
+    <h2>Cadastro</h2>
+    <input id="nome" placeholder="Nome"><br><br>
+    <input id="email" placeholder="Email"><br><br>
+    <input id="senha" type="password" placeholder="Senha"><br><br>
+    <button onclick="cadastrar()">Criar Conta</button>
+  `;
+}
+
+function cadastrar() {
+  let usuarios = carregar("usuarios");
+
+  let novo = {
+    id: Date.now(),
+    nome: nome.value,
+    email: email.value,
+    senha: senha.value,
+    fazenda: {
+      tanques: [],
+      financeiro: [],
+      vendas: []
+    }
+  };
+
+  usuarios.push(novo);
+  salvar("usuarios", usuarios);
+  alert("Conta criada!");
+  telaLogin();
+}
+
+function login() {
+  let usuarios = carregar("usuarios");
+
+  let user = usuarios.find(u =>
+    u.email === email.value &&
+    u.senha === senha.value
+  );
+
+  if (!user) {
+    alert("Usuário não encontrado");
+    return;
+  }
+
+  usuarioAtual = user;
+  salvar("usuarioLogado", user);
+  iniciarSistema();
+}
+
+function verificarLogin() {
+  let salvo = carregar("usuarioLogado");
+  if (salvo) {
+    usuarioAtual = salvo;
+    iniciarSistema();
+  } else {
+    telaLogin();
+  }
+}
