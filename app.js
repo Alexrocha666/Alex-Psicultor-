e59557a7aba534103de8c3244fb357476c8cb563
Alexrocha666 <alexpinposo@gmail.com>
2026-02-23 22:44:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ function iniciarSistema() {
   document.getElementById("app").innerHTML = `
     <h1>🐟 Alex Psicultor PRO</h1>
     <p>Bem-vindo, ${usuarioAtual.nome}</p>
-    <button onclick="logout()">Sair</button>
-  `;
-}
 
-function logout() {
-  localStorage.removeItem("usuarioLogado");
-  location.reload();
+    <button onclick="telaDashboard()">Dashboard</button>
+    <button onclick="telaTanques()">Tanques</button>
+    <button onclick="telaFinanceiro()">Financeiro</button>
+    <button onclick="telaVendas()">Vendas</button>
+    <button onclick="logout()">Sair</button>
+    <hr>
+  `;
 }
 
 verificarLogin();
