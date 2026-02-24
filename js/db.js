10765ce57e51b710811f98fefa1c76bd16d00cb6
Alexrocha666 <alexpinposo@gmail.com>
2026-02-23 22:55:08 -0300

    Delete js directory

--- js/db.js
+++
@@ -1,8 +0,0 @@
-const db = new Dexie("AlexPsicultorDB");
-
-db.version(1).stores({
-  tanques: "++id, nome, especie, quantidade, pesoInicial, metaPeso, dataPovoamento",
-  pesagens: "++id, tanqueId, data, pesoMedio",
-  financeiro: "++id, tipo, descricao, valor, data, tanqueId",
-  metas: "++id, tipo, valor"
-});
