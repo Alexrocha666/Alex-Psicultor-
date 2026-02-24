5425453a25a63a936f32b9a83174eda33cf85901
Alexrocha666 <alexpinposo@gmail.com>
2026-02-23 22:42:44 -0300

    Update storage.js

--- core/storage.js
+++ core/storage.js
@@ -5,3 +5,11 @@ function salvar(chave, dados) {
 function carregar(chave) {
   return JSON.parse(localStorage.getItem(chave)) || [];
 }
+
+function atualizarUsuarioGlobal(usuarioAtual) {
+  let usuarios = carregar("usuarios");
+  let index = usuarios.findIndex(u => u.id === usuarioAtual.id);
+  usuarios[index] = usuarioAtual;
+  salvar("usuarios", usuarios);
+  salvar("usuarioLogado", usuarioAtual);
+}
